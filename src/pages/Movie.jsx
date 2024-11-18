4cfc609fb437a7ad44c33d102c89fb8f74694282
ekahanny <ekahnny@gmail.com>
2024-11-18 16:23:58 +0800

    add: loading bars

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -7,44 +7,61 @@ import tmdbApi, { movieType } from "../api/tmdbApi";
 export function Movie() {
   const [movies, setMovies] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(movieType.popular);
+  const [loading, setLoading] = useState(false);
 
   // Fetch API
   useEffect(() => {
     const fetchMovies = async () => {
+      setLoading(true);
       try {
         const response = await tmdbApi.getMovies(selectedCategory, {
           language: "en-US",
           page: 1,
         });
+        await new Promise((resolve) => setTimeout(resolve, 3000));
         setMovies(response.results);
       } catch (error) {
         console.error("Error fetching movies:", error);
+      } finally {
+        setLoading(false);
       }
     };
-
     fetchMovies();
   }, [selectedCategory]);
+
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <NavBar />
-      <label className="form-control w-full max-w-xs mt-4 mx-8">
-        <div className="label">
-          <span className="label-text text-white font-semibold">
-            Select Category
-          </span>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <span className="loading loading-bars loading-lg text-info"></span>
         </div>
-        <select
-          className="select select-bordered"
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-        >
-          <option value={movieType.popular}>Popular</option>
-          <option value={movieType.now_playing}>Now Playing</option>
-          <option value={movieType.top_rated}>Top Rated</option>
-          <option value={movieType.upcoming}>Upcoming</option>
-        </select>
-      </label>
-      <MovieCard items={movies} titleKey="title" release_date="release_date" />
+      ) : (
+        <>
+          <label className="form-control w-full max-w-xs mt-4 mx-8">
+            <div className="label">
+              <span className="label-text text-white font-semibold">
+                Select Category
+              </span>
+            </div>
+            <select
+              className="select select-bordered"
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+            >
+              <option value={movieType.popular}>Popular</option>
+              <option value={movieType.now_playing}>Now Playing</option>
+              <option value={movieType.top_rated}>Top Rated</option>
+              <option value={movieType.upcoming}>Upcoming</option>
+            </select>
+          </label>
+          <MovieCard
+            items={movies}
+            titleKey="title"
+            release_date="release_date"
+          />
+        </>
+      )}
       <Footer />
     </div>
   );
--- src/pages/TVShow.jsx
+++ src/pages/TVShow.jsx
@@ -7,47 +7,60 @@ import tmdbApi, { tvType } from "../api/tmdbApi";
 export function TVShow() {
   const [tvShows, setTvShows] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(tvType.popular);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchTvShows = async () => {
+      setLoading(true);
       try {
         const response = await tmdbApi.getTvShows(selectedCategory, {
           language: "en-US",
           page: 1,
         });
+        await new Promise((resolve) => setTimeout(resolve, 3000));
         setTvShows(response.results);
       } catch (error) {
         console.error("Error fetching TV shows:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchTvShows();
   }, [selectedCategory]);
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <NavBar />
 
-      <label className="form-control w-full max-w-xs mt-4 mx-8">
-        <div className="label">
-          <span className="label-text text-white font-semibold">
-            Select Category
-          </span>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <span className="loading loading-bars loading-lg text-info"></span>
         </div>
-        <select
-          className="select select-bordered"
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-        >
-          <option value={tvType.popular}>Popular</option>
-          <option value={tvType.top_rated}>Top Rated</option>
-          <option value={tvType.on_the_air}>On The Air</option>
-        </select>
-      </label>
-      <MovieCard
-        items={tvShows}
-        titleKey="name"
-        release_date="first_air_date"
-      />
+      ) : (
+        <>
+          <label className="form-control w-full max-w-xs mt-4 mx-8">
+            <div className="label">
+              <span className="label-text text-white font-semibold">
+                Select Category
+              </span>
+            </div>
+            <select
+              className="select select-bordered"
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+            >
+              <option value={tvType.popular}>Popular</option>
+              <option value={tvType.top_rated}>Top Rated</option>
+              <option value={tvType.on_the_air}>On The Air</option>
+            </select>
+          </label>
+          <MovieCard
+            items={tvShows}
+            titleKey="name"
+            release_date="first_air_date"
+          />
+        </>
+      )}
       <Footer />
     </div>
   );
